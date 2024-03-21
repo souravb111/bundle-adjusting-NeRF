53e4e947fa4c74e9397a6b2cdd33c8c2d57df124
Ben Agro <bagro@waabi.ai>
2024-03-21 14:20:44 +0000

    slight fix

--- model/nerf.py
+++ model/nerf.py
@@ -81,13 +81,12 @@ class Model(base.Model):
                 temp_var = {}
                 proportion_accessible = self.it / self.image_sched_max_iters
                 mask = var['time'] < max(self.image_sched_min_time, proportion_accessible)
-                old_mask = var['time'] < max(self.image_sched_min_time, (self.it - 1) / self.image_sched_max_iters)
 
-                if mask.sum() != old_mask.sum() or (self.it == 0): # visualize poses
+                if (int(mask.shape[0] * (self.it / self.image_sched_max_iters)) !=  int(mask.shape[0] * ((self.it - 1)/ self.image_sched_max_iters))) or (self.it == 0): # visualize poses
                     fig = plt.figure(figsize=(10,10) if opt.data.dataset=="blender" else (16,8))
                     cam_path = "{}/poses".format(opt.output_path)
                     os.makedirs(cam_path,exist_ok=True)
-                    pose,pose_ref = self.get_all_training_poses(opt)
+                    pose,_ = self.get_all_training_poses(opt)
                     _ = util_vis.plot_save_poses(opt,fig,pose.cpu(),pose_ref=None,path=cam_path,ep=self.it,cam_depth=1.0)
                     # rgb = torch.tensor(imageio.imread(png_fname)).permute(2,0,1)
                     # util_vis.tb_image(opt,self.tb,self.it,"train","poses",rgb)
